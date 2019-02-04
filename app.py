85eb88be08c316d52dbf7713bea729bc55cad4e3
SalwaAhmedfci <37433010+SalwaAhmedfci@users.noreply.github.com>
2019-02-04 15:12:23 +0200

    Enhancement of fuzzy ratio

--- app.py
+++ app.py
@@ -2,29 +2,70 @@
 
 # !/usr/bin/env python
 from bs4 import BeautifulSoup
-from userAgent import randomUserAgents
-from flask import Flask, render_template, request
 from database_setup import *
-import requests
 from database_setup import *
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from excrating import *
-from sqlalchemy.orm import load_only
 import sqlite3
 import re
-#
-#
-app = Flask(__name__)
-
-# def extractnum(input):
-#     numbers = re.sub(r'[a-z]+', '', input, re.I)
-#     return numbers
 
 
-def get_company_name():
-    companyname = request.form['company_name']
-    return companyname
+
+def scrape_company_info(name):
+    companyname = name
+    head = randomUserAgents()
+    # scraping companyrating
+    page1_link = "https://www.glassdoor.com/Reviews/company-reviews.htm?suggestCount=0&suggestChosen=false&clickSource" \
+                 "=searchBtn&typedKeyword='{0}'&sc.keyword='{1}'&locT=&locId=&jobType=".format(
+        companyname, companyname)
+    bs = soup(page1_link, head)
+
+    mylist = []
+    matched_companies = []
+
+    main_div = bs.findAll('div', {'class': "eiHdrModule module snug"})
+    for rate in main_div:
+        if rate.find('span', {'class': "bigRating strong margRtSm h2"}) is not None:
+            mylist.append(rate.find('span', {'class': "bigRating strong margRtSm h2"}).get_text())
+        else:
+            mylist.append("Not rated yet")
+        matched_companies.append(rate.find('a', {'class': "tightAll h2"}))
+    # loop of printing
+
+    mylist_names = []
+    mylist_rates = []
+    count = 0
+    while count < matched_companies.__len__():
+        mylist_names.append(matched_companies[count].get_text())
+        mylist_rates.append(mylist[count])
+        count = count + 1
+    # scraping price
+    page2_link = "https://finance.yahoo.com/quote/{0}?p={1}&.tsrc=fin-srch".format(companyname, companyname)
+    head1 = randomUserAgents()
+    bs2 = soup(page2_link, head1)
+    # cell = bs2.select("span[data-reactid ='39']")
+    # data - test = "PREV_CLOSE-value"
+    close = []
+
+    p = bs2.find("td", {"data-test": "PREV_CLOSE-value"})
+    # exract numbers from text
+    # price = p.replace("Previous Close", "")
+    # print(p.get_text())
+    info = get_company_info(companyname)
+    # showing results
+    price = p.get_text()
+    array = []
+    array.append(mylist_rates)
+    array.append(mylist_names)
+    array.append(price)
+    array.append(info)
+    return array
+
+
+def get_company_name(name):
+    companyname = name
+    return scrape_company_info(companyname)
 
 def soup(url, headers):
     session = requests.Session()
@@ -32,30 +73,36 @@ def soup(url, headers):
     bs = BeautifulSoup(req.text, 'html.parser')
     return bs
 
-def get_company_info():
+def get_company_info(name):
     companyinfo = []
+    enhancement_ratio = 50
+    companyname = name
     try:
-        companyname = get_company_name()
-        print(companyname)
-            # pick up the target company
+        # pick up the target company
         company = session.query(Company).filter_by(
         name=companyname).one()
-            # fuzzy match here
+        # fuzzy match here
         query = company.industry
         allindustries = []
-            # load only indusrty column from look_up
+                # load only indusrty column from look_up
         industries = session.query(look_up).all()
         for ind in industries:
-            allindustries.append(ind.industry)
+
+            if fuzz.ratio(query,ind.industry) > enhancement_ratio :
+
+                allindustries.append(ind.industry)
+            else:
+                continue
         #do fuzzuy matching
+
         industry = process.extract(query, allindustries)
         #print(industry)
         matched_industry = []
         for i in industry:
             matched_industry.append(i[0])
         #print(matched_industry)
-            # pick up the matched company indusrty and SIC
-            #print(matched_industry)
+        # pick up the matched company indusrty and SIC
+        #print(matched_industry)
 
         conn = sqlite3.connect('CompainesData.db')
         c = conn.cursor()
@@ -77,7 +124,7 @@ def get_company_info():
         return companyinfo
     except:
 
-        companyinfo.append(get_company_name())
+        companyinfo.append(companyname)
         companyinfo.append("not covered yet")
         companyinfo.append("not covered yet")
         companyinfo.append("not covered yet")
@@ -88,59 +135,11 @@ def get_company_info():
 
 
 
-@app.route('/', methods=['POST', 'GET'])
-def my_form_post():
-
-    if request.method == 'GET':
-
-        return render_template("welcome.html")
-    elif request.method == 'POST':
-        companyname = get_company_name()
-        head = randomUserAgents()
-    #scraping companyrating
-        page1_link = "https://www.glassdoor.com/Reviews/company-reviews.htm?suggestCount=0&suggestChosen=false&clickSource" \
-                "=searchBtn&typedKeyword='{0}'&sc.keyword='{1}'&locT=&locId=&jobType=".format(
-        companyname, companyname)
-        bs = soup(page1_link, head)
-
-        mylist = []
-        matched_companies = []
-
-        main_div = bs.findAll('div', {'class': "eiHdrModule module snug"})
-        for rate in main_div:
-            if rate.find('span', {'class': "bigRating strong margRtSm h2"}) is not None:
-                mylist.append(rate.find('span', {'class': "bigRating strong margRtSm h2"}).get_text())
-            else:
-                mylist.append("Not rated yet")
-            matched_companies.append(rate.find('a', {'class': "tightAll h2"}))
-        # loop of printing
-
-        mylist_names = []
-        mylist_rates = []
-        count = 0
-        while count < matched_companies.__len__():
-            mylist_names.append(matched_companies[count].get_text())
-            mylist_rates.append(mylist[count])
-            count = count + 1
-        #scraping price
-        page2_link ="https://finance.yahoo.com/quote/{0}?p={1}&.tsrc=fin-srch".format(companyname,companyname)
-        head1 = randomUserAgents()
-        bs2 = soup(page2_link, head1)
-        #cell = bs2.select("span[data-reactid ='39']")
-        #data - test = "PREV_CLOSE-value"
-        close =[]
-
-        p =bs2.find("td", {"data-test": "PREV_CLOSE-value"})
-        #exract numbers from text
-        #price = p.replace("Previous Close", "")
-        #print(p.get_text())
-        info=get_company_info()
-        #showing results
-        return render_template("show.html", companyname=companyname, mylist_rates=mylist_rates, mylist_names=mylist_names, price =p.get_text(), info =info)
 
 
-# print (rate)
+name = input('enter the company name:')
+print(get_company_name(name))
 
-if __name__ == '__main__':
 
-     app.run(debug=True)
+
+

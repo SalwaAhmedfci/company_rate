d33cf59560b381f1191196654a418eadc3af34eb
SalwaAhmedfci <37433010+SalwaAhmedfci@users.noreply.github.com>
2019-02-07 14:43:06 +0200

    adding the industry to gui

--- GUI.py
+++ GUI.py
@@ -8,10 +8,12 @@ from tabulate import tabulate
 msgbox("Welcome to Company Info App",image='profile.png')
 name = enterbox("Enter the company name:", "Info")
 msgbox("Welcome to  " + name)
-
-msgbox("profits : "+get_profit(name),"profit")
-msgbox("marketvalue :"+get_marketvalue(name),"marketvalue")
-msgbox("revenue :" +get_reveune(name),"revenue")
+tableinfo=[]
+tableinfo.append(["profits : " ,get_profit(name)])
+tableinfo.append(["marketvalue :  " ,get_marketvalue(name)])
+tableinfo.append(["revenue :  " ,get_reveune(name)])
+tableinfo.append(["price : " ,get_price(name)])
+msgbox(tabulate(tableinfo),"Company Info ")
 array = get_company_rating(name)
 count = 0
 rates = array[0]
@@ -23,35 +25,11 @@ while count < rates.__len__():
     count=count+1
 msgbox(tabulate(table),"Company Rating ")
 ind =get_company_indusrty(name)
-count = 1
-table1 = []
-if(ind[0].__len__()==2):
-    while count < ind[0].__len__():
-        table1.append([ind[0][count][0][count], ind[0][0][0][0]])
-        table1.append([ind[0][0][0][count], ind[0][count][0][0]])
-        msgbox(tabulate(table1), "industry and SIC ")
-        count = count + 1
-# elif((ind[0].__len__()==3)):
-#
-#     msgbox(tabulate(table1), "industry and SIC ")
-# elif((ind[0].__len__()==4)):
-#     count = 1
-#     while count < ind[0].__len__():
-#         table1.append([ind[0][1][0][0], ind[0][0][0][0]])
-#         table1.append([ind[0][0][0][1], ind[0][1][0][0]])
-#         table1.append([ind[0][1][0][2], ind[0][2][0][0]])
-#         table1.append([ind[0][1][0][3], ind[0][3][0][0]])
-#         count =count+1
-#     msgbox(tabulate(table1), "industry and SIC ")
-# elif((ind[0].__len__()==5)):
-#
-#     msgbox(tabulate(table1), "industry and SIC ")
-else:
 
-    msgbox(get_company_indusrty(name),"industry and SIC ")
 
-# industry
+msgbox(get_company_indusrty(name),"industry and SIC ")
 
 
 
-msgbox("price : " +get_price(name),"price")
+
+
